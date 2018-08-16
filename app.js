22efb4216b50a51782cada79ff69014d1ea1d2a5
Naveen Koti <naveenkoti@gmail.com>
2018-08-16 18:02:59 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBfBXS_hVqSsnNF1bbNNup4ycT1xYGam20",
+    authDomain: "ilive-51b7c.firebaseapp.com",
+    databaseURL: "https://ilive-51b7c.firebaseio.com",
+    projectId: "ilive-51b7c",
+    storageBucket: "ilive-51b7c.appspot.com",
+    messagingSenderId: "49552927249"
+  };
+  firebase.initializeApp(config);
